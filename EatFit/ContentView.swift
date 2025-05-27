ab1eb2ee4b2d2d8b2a3016b01c2bbd6c5d3bcfcb
abhilashg92 <ghogaleabhilash92@gmail.com>
2025-05-27 17:48:57 +0530

    Initial Commit

---
+++ EatFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EatFit
+//
+//  Created by Abhilash Ghogale on 27/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EatFit/EatFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatFitApp.swift
+//  EatFit
+//
+//  Created by Abhilash Ghogale on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct EatFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
